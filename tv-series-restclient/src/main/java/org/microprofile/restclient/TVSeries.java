f712f529bf1613b8702b99f32acb6a6e23f17804
Sunil Popat Thitame <72607944+sunilthitme@users.noreply.github.com>
2023-11-05 18:02:38 +0530

    Added

---
+++ tv-series-restclient/src/main/java/org/microprofile/restclient/TVSeries.java
@@ -0,0 +1,52 @@
+package org.microprofile.restclient;
+
+import java.net.URL;
+//import jakarta.persistence.Entity;
+
+
+public class TVSeries {
+    int id;
+    URL url;
+    String name;
+    String type;
+    String language;
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
